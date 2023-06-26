2575707a539e80e6f57aec3aeb2def4134ebb7f4
nehalsahukar19 <96016289+nehalsahukar19@users.noreply.github.com>
2023-06-26 21:32:46 +0530

    Add files via upload

--- model.py
+++ model.py
@@ -2,7 +2,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
+from sklearn import metrics
 import pickle
+from xgboost import XGBRegressor
 
 def train_model():
     # Load the Boston Housing dataset
@@ -16,7 +18,8 @@ def train_model():
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     # Train the linear regression model
-    model = LinearRegression()
+    model = XGBRegressor()
+    # model = LinearRegression()
     model.fit(X_train, y_train)
 
     # Save the trained model as a pickle file
@@ -24,10 +27,12 @@ def train_model():
         pickle.dump(model, f)
 
     # Evaluate the model on the test set
-    y_pred = model.predict(X_test)
-    mse = mean_squared_error(y_test, y_pred)
-    return mse
+    y_test_pred = model.predict(X_test)
+    # mse = mean_squared_error(y_test, y_pred)
+    acc_xgb = metrics.r2_score(y_test, y_test_pred)
+    return acc_xgb 
+
 
 if __name__ == '__main__':
-    mse = train_model()
-    print(f'Mean Squared Error: {mse}')
+    acc_xgb = train_model()
+    print(f'Accuracy: {acc_xgb}')
